ce2d773333959c3bd2a8fe00225454ef6446682c
RAZE <razea1.the.master@gmail.com>
2025-08-19 19:12:32 -0700

    navigation bar

--- app.tsx
+++ app.tsx
@@ -3,20 +3,41 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantWebView from "./screens/RestaurantWebView";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SettingsScreen from "./screens/settings";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: "Yarka Restaurants" }} />
+      <Stack.Screen name="RestaurantWebView" component={RestaurantWebView} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Yarka Restaurants" component={HomeScreen} />
-        <Stack.Screen
-          name="RestaurantWebView"
-          component={RestaurantWebView}
-          options={({ route }) => ({ title: (route.params as { name: string }).name })}
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
+          }}
         />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => <Ionicons name="settings" size={size} color={color} />,
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
